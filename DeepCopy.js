0ed83bf6f62aeb2123133ab9c3f6f64c58a04bbb
BABITA <babitaarya71@gmail.com>
2026-05-01 04:16:41 +0530

    updated code

---
+++ DeepCopy.js
@@ -0,0 +1,12 @@
+
+function deepCopy(obj) {
+    if (obj === null || typeof obj !== 'object')
+        return obj;
+    let copy = (obj.constructor === Array) ? [] : {}
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            copy[key] = deepCopy(obj[key])
+        }
+    }
+    return copy
+}
