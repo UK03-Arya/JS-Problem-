f71a871d1939c2087255397418ce6a55a8c49080
BABITA <babitaarya71@gmail.com>
2026-05-05 08:00:57 +0530

    update code

--- lowerUpperSwap.js
+++ lowerUpperSwap.js
@@ -1,19 +1,38 @@
-function sortContacts(arr) {
-    let upper = [], lower = [];
 
-    // Sirf EK loop: Contacts ko categories mein divide karne ke liye
+//phle lower and upper ko alag karna hai, phir sort karna hai
+
+
+function contactsOnly(arr) {
+    let upper = [], lower = []
     for (let i = 0; i < arr.length; i++) {
-        let name = arr[i];
-        if (name[0] >= 'A' && name[0] <= 'Z') {
-            upper[upper.length] = name;
-        } else {
-            lower[lower.length] = name;
+        const name = arr[i]
+        if (name[0] >= 'A' && name[0] <= ' Z') {
+            upper[upper.length] = name
+        }
+        else {
+            lower[lower.length] = name
         }
     }
 
-    // Ab yahan sorting karni hai.
-    // Note: Bina built-in .sort() ke, sort karne ke liye loop lagta hi hai.
-    // Lekin agar aapka question sirf "Separation" par hai, toh ye best hai.
+    for (let i = 0; i < upper.length; i++) {
+        for (let j = 0; j < upper.length - 1 - i; j++) {
+            if (upper[j] > upper[j + 1]) {
+                let temp = upper[j]
+                upper[j] = upper[j + 1]
+                upper[j + 1] = temp
+            }
+        }
+    }
 
-    return [...upper, ...lower];
+    for (let i = 0; i < lower.length; i++) {
+        for (let j = 0; j < lower.length - 1 - i; j++) {
+            if (lower[j] > lower[j + 1]) {
+                let temp = lower[j]
+                lower[j] = lower[j + 1]
+                lower[j + 1] = temp
+            }
+        }
+    }
+
+    return [...upper, ...lower]
 }
