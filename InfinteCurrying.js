052410f3c7efc257fa2f50c4fd1123c344e7d2c7
BABITA <babitaarya71@gmail.com>
2026-05-06 20:06:54 +0530

    updated code

---
+++ InfinteCurrying.js
@@ -0,0 +1,14 @@
+function sum(a) {
+    return function (b) {
+        // Agar 'b' exist karta hai, toh recursion chalao
+        if (b !== undefined) {
+            return sum(a + b);
+        }
+        // Agar argument nahi hai, toh total return karo
+        return a;
+    };
+}
+
+// Usage:
+console.log(sum(1)(2)(3)(4)()); // Output: 10
+console.log(sum(10)(20)());     // Output: 30
